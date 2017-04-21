d9fe2f4a35e9a4370d807cccaf4bbf42c660c1ff
Thuc Huynh <hdthuc93@gmail.com>
2017-04-22 00:31:22 +0700

    clear cache

--- src/server/config/connection.js
+++
@@ -1,9 +0,0 @@
-export default {
-    hostDB: 'localhost',
-    portDB: 3306,
-    nameDB: 'student_management_schema',
-    userDB: 'root',
-    passwordDB: '123456',
-    secretKey: '',
-    port: 3000
-};
